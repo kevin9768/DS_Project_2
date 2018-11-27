c6bea00906c4722334c0a952d13f47d6f9f1ba2c
Kevin <kevin.shih.fr@gapp.nthu.edu.tw>
2018-11-27 18:17:17 +0800

    alpha 0.0.3 introduced bfs

--- robot.cpp
+++ robot.cpp
@@ -3,17 +3,19 @@
 #include<cstring>
 #include<vector>
 #include<stack>
+#include<queue>
 #include<cstdlib>
-#include<cmath>
 
 using namespace std;
 
 struct point{
     int x,y;
     point(int a,int b):x(a),y(b){}
+    point():x(0),y(0){}
 };
 
 bool **available;
+bool **init_clean;
 bool **clean;
 bool **final_clean;
 int  **mindistfrominit;
@@ -25,7 +27,9 @@ vector<point> order;
 vector<point> last_order;
 
 
-void dfs(int r, int c, int cost);
+void dfs(int r, int c);
+void bfs(int r, int c);
+void clean_data();
 void fixmin();
 inline bool isAdj(point a, point b);
 void gotobattery(int x, int y);
@@ -44,12 +48,14 @@ int main(int argc, char* argv[]){
     inFile >> row >> col >> life;
 
     available  = new bool* [row];
-    clean  = new bool* [row];
+    init_clean  = new bool* [row];
+    clean = new bool* [row];
     mindistfrominit = new int* [row];
     final_clean = new bool* [row];
     for(int i = 0; i < row; i++){
         available[i]  = new bool [col];
-        clean[i]  = new bool [col];
+        init_clean[i]  = new bool [col];
+        clean[i] = new bool [col];
         mindistfrominit[i] = new int [col];
         final_clean[i] = new bool [col];
     }
@@ -57,18 +63,20 @@ int main(int argc, char* argv[]){
     //setup 2d-arrays
     for(int i = 0; i < row; i++)
         for(int j = 0; j < col; j++){
-            clean[i][j]  = false;
+            init_clean[i][j]  = false;
             final_clean[i][j] = false;
-            mindistfrominit[i][j] = 3000;
+            mindistfrominit[i][j] = 498600;
             char c;
             inFile >> c;
-            if(c == '1')
+            if(c == '1'){
                 available[i][j] = false;  
+                init_clean[i][j]  = true;
+            }
             else{
                 available[i][j] = true;
                 if(c == 'R'){
                     r_row = i, r_col = j;
-                    clean[i][j]  = true;
+                    init_clean[i][j]  = true;
                     final_clean[i][j] = false;
                     available[i][j] = false;
                 }
@@ -76,20 +84,32 @@ int main(int argc, char* argv[]){
         }
     
     //find out the order to go through these point
+    int dir=0;
+    while(1){
+        bool cool = true;
+        init_row=r_row, init_col=r_col;
+        //determine which point to go out through starting point
+        if(     init_col<col-1  &&  available[init_row][init_col+1] && dir<1)      init_col++,dir=1;
+        else if(init_row<row-1  &&  available[init_row+1][init_col] && dir<2)      init_row++,dir=2;
+        else if(init_col>0      &&  available[init_row][init_col-1] && dir<3)      init_col--,dir=3;
+        else if(init_row>0      &&  available[init_row-1][init_col] && dir<4)      init_row--,dir=4;
+        clean_data();
+        bfs(init_row, init_col);
 
-    init_row=r_row, init_col=r_col;
-    //determine which point to go out through starting point
-    if(     init_col<col-1  &&  available[init_row][init_col+1])      init_col++;
-    else if(init_row<row-1  &&  available[init_row+1][init_col])      init_row++;
-    else if(init_col>0      &&  available[init_row][init_col-1])      init_col--;
-    else if(init_row>0      &&  available[init_row-1][init_col])      init_row--;
+        for(int i=0; i<row; i++)
+            for(int j=0; j<col; j++)
+                if((mindistfrominit[i][j]+1)*2>life&&available[i][j])
+                    cool = false;
+        
+        if(cool==true) break;
 
-    mindistfrominit[init_row][init_col]=0;
-    dfs(init_row, init_col, 0);
-    fixmin();
-    fixmin();
-    fixmin();
-    fixmin();
+        for(int i=0; i<row; i++)
+            for(int j=0; j<col; j++)
+                mindistfrominit[i][j]=498600;
+    }
+    //cout<<"1\n";
+    clean_data();
+    dfs(init_row, init_col);
     
 
     /*for(int i=0;i<row;i++){           //function for seeing if minimum distance is working
@@ -101,7 +121,6 @@ int main(int argc, char* argv[]){
 
 
     //run through order
-    //cout<<order[0].x<<' '<<order[0].y<<'\n';
     last_order.push_back(order[0]);
     final_clean[order[0].x][order[0].y]=true;
     int bt=life;
@@ -153,17 +172,16 @@ int main(int argc, char* argv[]){
     }
 }
 
-void dfs(int r, int c, int cost){
+void dfs(int r, int c){
     clean[r][c]=true;
     if(!available[r][c]) return;
-    if(cost<mindistfrominit[r][c]) mindistfrominit[r][c]=cost;
     //cout<<r<<' '<<c<<'\n';
     point p(r,c);
     order.push_back(p);
-    if(r>0&&!clean[r-1][c])           dfs(r-1,c,cost+1);
-    if(c>0&&!clean[r][c-1])           dfs(r,c-1,cost+1);
-    if(r<row-1&&!clean[r+1][c])       dfs(r+1,c,cost+1);
-    if(c<col-1&&!clean[r][c+1])       dfs(r,c+1,cost+1);
+    if(r>0&&!clean[r-1][c])           dfs(r-1,c);
+    if(c>0&&!clean[r][c-1])           dfs(r,c-1);
+    if(r<row-1&&!clean[r+1][c])       dfs(r+1,c);
+    if(c<col-1&&!clean[r][c+1])       dfs(r,c+1);
 }
 
 void fixmin(){
@@ -257,4 +275,63 @@ int getback(int x, int y){
 
     return life-mindistfrominit[tox][toy]-1;
     
+}
+
+void bfs(int s_r,int s_c){ 
+    queue <point> q;
+    point u,upos,in;
+    u.x=s_r;
+    u.y=s_c;
+    mindistfrominit[u.x][u.y]=0;
+    clean[u.x][u.y]=true;
+ 
+    q.push(u);
+    while(!q.empty()){ 
+        upos=q.front();
+        //cout<<upos.x<<" "<<upos.y<<endl;
+        q.pop();
+ 
+        if(upos.x>0&&available[upos.x-1][upos.y]&&!clean[upos.x-1][upos.y]){
+            in.x=upos.x-1;
+            in.y=upos.y;
+            mindistfrominit[upos.x-1][upos.y]=mindistfrominit[upos.x][upos.y]+1;
+            q.push(in);
+            clean[in.x][in.y]=true;
+            
+        }
+ 
+        if(upos.x+1<row&&available[upos.x+1][upos.y]&&!clean[upos.x+1][upos.y]){
+            in.x=upos.x+1;
+            in.y=upos.y;
+            mindistfrominit[upos.x+1][upos.y]=mindistfrominit[upos.x][upos.y]+1;
+            q.push(in);
+            clean[in.x][in.y]=true;
+        }
+ 
+        if(upos.y>0&&available[upos.x][upos.y-1]&&!clean[upos.x][upos.y-1]){
+            in.x=upos.x;
+            in.y=upos.y-1;
+            mindistfrominit[upos.x][upos.y-1]=mindistfrominit[upos.x][upos.y]+1;
+            q.push(in);
+            clean[in.x][in.y]=true; 
+        }
+ 
+        if(upos.y+1<col&&available[upos.x][upos.y+1]&&!clean[upos.x][upos.y+1]){
+            in.x=upos.x;
+            in.y=upos.y+1;
+            mindistfrominit[upos.x][upos.y+1]=mindistfrominit[upos.x][upos.y]+1;
+            q.push(in); 
+            clean[in.x][in.y]=true;
+        }
+ 
+    }
+ 
+    //printf("%d\n",mindistfrominit[d_r][d_c]);
+ 
+}
+
+void clean_data(){
+    for(int i=0; i<row; i++)
+        for(int j=0; j<col; j++)
+            clean[i][j] = init_clean[i][j];
 }
